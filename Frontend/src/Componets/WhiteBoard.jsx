5e2e9e4c7e8f069a53023ec7e20c17989469064c
Adarsh Learner <learner.adarsh@gmail.com>
2024-10-16 11:22:06 +0530

    formatting done

--- Frontend/src/Componets/WhiteBoard.jsx
+++ Frontend/src/Componets/WhiteBoard.jsx
@@ -10,26 +10,29 @@ const WhiteBoard = () => {
 
     return <>
         <div style={{ height: "100vh", width: "100%" }}>
-            <Excalidraw gridModeEnabled={gridMode} renderTopRightUI={() => (
+        <Excalidraw gridModeEnabled={gridMode} renderTopRightUI={() => (
                     <button onClick={toggleGridBackground} style={{ padding: '8px', marginRight: '8px' }}>
                         {gridMode ? "Disable Grid" : "Enable Grid"}
                     </button>
-                )}><WelcomeScreen>
+                )}>
+            <WelcomeScreen>
                 <WelcomeScreen.Hints.ToolbarHint>
                   <p> ToolBar Hints </p>
                 </WelcomeScreen.Hints.ToolbarHint>
                 <WelcomeScreen.Hints.MenuHint />
                 <WelcomeScreen.Hints.HelpHint />
-              </WelcomeScreen><MainMenu>
-          <MainMenu.DefaultItems.Export />
-          <MainMenu.DefaultItems.LiveCollaborationTrigger/>
-          <MainMenu.DefaultItems.SaveAsImage/>
-          <MainMenu.DefaultItems.ChangeCanvasBackground/>
-          <MainMenu.DefaultItems.SaveToActiveFile/>
-          <MainMenu.DefaultItems.ToggleTheme/>
-          <MainMenu.DefaultItems.ClearCanvas/>
-          <MainMenu.DefaultItems.Help/>
-        </MainMenu></Excalidraw>
+            </WelcomeScreen>
+            <MainMenu>
+                <MainMenu.DefaultItems.Export />
+                <MainMenu.DefaultItems.LiveCollaborationTrigger/>
+                <MainMenu.DefaultItems.SaveAsImage/>
+                <MainMenu.DefaultItems.ChangeCanvasBackground/>
+                <MainMenu.DefaultItems.SaveToActiveFile/>
+                <MainMenu.DefaultItems.ToggleTheme/>
+                <MainMenu.DefaultItems.ClearCanvas/>
+                <MainMenu.DefaultItems.Help/>
+            </MainMenu>
+        </Excalidraw>
         </div>
     </>
 };
