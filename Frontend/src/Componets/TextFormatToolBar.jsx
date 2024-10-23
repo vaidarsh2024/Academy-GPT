4c046aef8b4e138402087468de37fef97ac12f86
Adarsh Learner <learner.adarsh@gmail.com>
2024-10-23 18:56:32 +0530

    done

--- Frontend/src/Componets/TextFormatToolBar.jsx
+++ Frontend/src/Componets/TextFormatToolBar.jsx
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 import { FaBold, FaItalic, FaAlignLeft, FaAlignCenter, FaAlignRight, FaAlignJustify } from 'react-icons/fa';
 import { MdFormatSize } from 'react-icons/md';
 import './TextFormatToolBar.css'; // Add your custom CSS
+import bold from '../assets/Image/bold.svg';
+import frame from '../assets/Image/frame.svg';
+import italic from '../assets/Image/italic.svg';
+import link from '../assets/Image/link.svg';
 
 export const TextFormatToolBar = ({ onBold, onItalic, onFontSizeChange, onAlign }) => {
   return (
-    <div className="toolbar" style={{backgroundColor: '#27292C', padding: '8px', height: 'min-content', display: 'flex', flexDirection: 'column', marginTop: '16px', color: '#fff', border: '1px solid #ccc'}}>
-      <button onClick={onBold} title="Bold">
-        <FaBold />
+    <div className="toolbar" style={{backgroundColor: '#27292C', padding: '4px', height: 'min-content', display: 'flex', flexDirection: 'column', marginTop: '16px', color: '#fff', border: '1px solid #ccc', alignItems: 'center'}}>
+      <button onClick={() => onFontSizeChange('medium')} title="Medium Font" style={{marginTop: '20px'}}>
+        <img src={link} />
       </button>
-      <button onClick={onItalic} title="Italic">
-        <FaItalic />
+      <button onClick={() => onFontSizeChange('small')} title="Small Font" style={{marginTop: '20px'}}>
+        <img src={frame} />
       </button>
-      <button onClick={() => onFontSizeChange('small')} title="Small Font">
-        <MdFormatSize />
+      <button onClick={onItalic} title="Italic" style={{marginTop: '20px'}}>
+        <img src={italic} />
       </button>
-      <button onClick={() => onFontSizeChange('medium')} title="Medium Font">
-        <MdFormatSize />
+      <button onClick={onBold} title="Bold" style={{marginTop: '20px'}}>
+        <img src={bold} />
       </button>
-      <button onClick={() => onFontSizeChange('large')} title="Large Font">
+      {/* <button onClick={() => onFontSizeChange('large')} title="Large Font">
         <MdFormatSize />
       </button>
       <button onClick={() => onAlign('left')} title="Align Left">
@@ -32,7 +36,7 @@ export const TextFormatToolBar = ({ onBold, onItalic, onFontSizeChange, onAlign 
       </button>
       <button onClick={() => onAlign('justify')} title="Justify">
         <FaAlignJustify />
-      </button>
+      </button> */}
     </div>
   );
 };
