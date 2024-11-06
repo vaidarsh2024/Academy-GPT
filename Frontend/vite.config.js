1aa1a5d3d6fd308fe162d580ebe686a0ef90538c
Adarsh Learner <learner.adarsh@gmail.com>
2024-11-06 18:10:18 +0530

    done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,5 +6,23 @@ export default defineConfig({
   define: {
     "process.env": {}
 },
+build: {
+  // Set the directory for assets
+  assetsDir: 'assets',
+
+  // Customize the filename pattern for assets
+  rollupOptions: {
+    output: {
+      assetFileNames: (assetInfo) => {
+        if (/\.(png|jpe?g|gif|svg|webp)$/.test(assetInfo.name)) {
+          // Use the original filename without hashing
+          return 'assets/[name].[ext]';
+        }
+        // For other assets, you can apply a different rule
+        return 'assets/[name]-[hash][extname]';
+      },
+    },
+  },
+},
   plugins: [react()],
 })
